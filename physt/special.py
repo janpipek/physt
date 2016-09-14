e8e6d81076b01dc44b2abf7af29fcee2426e1685
Jan Pipek <jan.pipek@gmail.com>
2016-09-14 10:11:28 +0200

    Update transform for polar, spherical and cylindrical

--- physt/special.py
+++ physt/special.py
@@ -18,6 +18,8 @@ class TransformedHistogramMixin(object):
     - `bin_sizes` property
 
     In certain cases, you may want to have default axis names + projections.
+    Look at PolarHistogram / SphericalHistogram / CylindricalHistogram as
+    an example.
     """
 
     @classmethod
@@ -28,6 +30,8 @@ class TransformedHistogramMixin(object):
         ----------
         value : array_like
             This method should accept both scalars and numpy arrays.
+            If multiple values are to be transformed, it should of
+            (nvalues, ndim) shape.
 
         Returns
         -------
@@ -63,9 +67,12 @@ class TransformedHistogramMixin(object):
 
 
 class PolarHistogram(TransformedHistogramMixin, HistogramND):
-    """Polar histogram data.
+    """2D histogram in polar coordinates.
 
-    This is a special case of a 2D histogram with transformed coordinates.
+    This is a special case of a 2D histogram with transformed coordinates:
+    - r as radius in the (0, +inf) range
+    - phi as azimuthal angle in the (0, 2*pi) range
+
     """
     def __init__(self, binnings, frequencies=None, **kwargs):
         if not "axis_names" in kwargs:
@@ -82,9 +89,12 @@ class PolarHistogram(TransformedHistogramMixin, HistogramND):
 
     @classmethod
     def transform(self, value):
-        r = np.hypot(value[1], value[0])
-        phi = np.arctan2(value[1], value[0]) % (2 * np.pi)
-        return (r, phi)
+        value = np.asarray(value, dtype=np.float64)
+        assert value.shape[-1] == 2
+        result = np.empty_like(value)
+        result[...,0] = np.hypot(value[...,1], value[...,0])
+        result[...,1] = np.arctan2(value[...,1], value[...,0]) % (2 * np.pi)
+        return result
 
     def projection(self, axis_name, **kwargs):
         if isinstance(axis_name, int):
@@ -134,6 +144,14 @@ class AzimuthalHistogram(Histogram1D):
 
 
 class SphericalHistogram(TransformedHistogramMixin, HistogramND):
+    """3D histogram in spherical coordinates.
+
+    This is a special case of a 2D histogram with transformed coordinates:
+    - r as radius in the (0, +inf) range
+    - theta as angle between z axis and the vector, in the (0, 2*pi) range
+    - phi as azimuthal angle  (in the xy projection) in the (0, 2*pi) range
+    """
+
     def __init__(self, binnings, frequencies=None, **kwargs):
         if not "axis_names" in kwargs:
             kwargs["axis_names"] = ("r", "theta", "phi")
@@ -142,19 +160,66 @@ class SphericalHistogram(TransformedHistogramMixin, HistogramND):
 
     @classmethod
     def transform(self, value):
-        x, y, z = value
+        value = np.asarray(value, dtype=np.float64)
+        result = np.empty_like(value)
+        x, y, z = value.T
         xy = np.hypot(x, y)
-        r = np.hypot(xy, z)
-        theta = np.arctan2(xy, z) % (2 * np.pi)
-        phi = np.arctan2(x, y) % (2 * np.pi)
-        return (r, theta, phi)
+        result[...,0] = np.hypot(xy, z)
+        result[...,1] = np.arctan2(xy, z) % (2 * np.pi)
+        result[...,2] = np.arctan2(y, x) % (2 * np.pi)
+        return result
 
     @property
     def bin_sizes(self):
-        sizes = 0.5 * (self.get_bin_right_edges(0) ** 2 - self.get_bin_left_edges(0) ** 2)
-        sizes2 = np.cos(self.get_bin_left_edges(1)) - np.cos(self.get_bin_right_edges(1))               # Hopefully correct
-        return reduce(np.multiply, np.ix_(sizes, sizes2, self.get_bin_widths(2)))
+        sizes1 = (self.get_bin_right_edges(0) ** 3 - self.get_bin_left_edges(0) ** 3) / 3
+        sizes2 = np.cos(self.get_bin_left_edges(1)) - np.cos(self.get_bin_right_edges(1))
+        sizes3 = self.get_bin_widths(2)
+         # Hopefully correct
+        return reduce(np.multiply, np.ix_(sizes1, sizes2,sizes3))
         #return np.outer(sizes, sizes2, self.get_bin_widths(2))    # Correct
+
+
+class CylindricalHistogram(TransformedHistogramMixin, HistogramND):
+    """3D histogram in cylindrical coordinates.
+
+    This is a special case of a 2D histogram with transformed coordinates:
+    - r as radius projection to xy plane in the (0, +inf) range
+    - phi as azimuthal angle  (in the xy projection) in the (0, 2*pi) range
+    - z as the last direction without modification, in (-inf, +inf) range
+    """
+    def __init__(self, binnings, frequencies=None, **kwargs):
+        if not "axis_names" in kwargs:
+            kwargs["axis_names"] = ("rho", "phi", "z")
+        kwargs.pop("dim", False)
+        super(CylindricalHistogram, self).__init__(3, binnings=binnings,
+              frequencies=frequencies, **kwargs)
+
+    @classmethod
+    def transform(self, value):
+        value = np.asarray(value, dtype=np.float64)
+        result = np.empty_like(value)
+        x, y, z = value.T
+        result[...,0] = np.hypot(x, y)                     # tho
+        result[...,1] = np.arctan2(y, x) % (2 * np.pi)     # phi
+        result[...,2] = z
+        return result
+
+    @property
+    def bin_sizes(self):
+        sizes1 = 0.5 * (self.get_bin_right_edges(0) ** 2 - self.get_bin_left_edges(0) ** 2)
+        sizes2 = self.get_bin_widths(1)
+        sizes3 = self.get_bin_widths(2)
+        return reduce(np.multiply, np.ix_(sizes1, sizes2, sizes3))
+
+
+def _prepare_data(data, transformed, klass,  *args, **kwargs):
+    data = np.asarray(data)
+    if not transformed:
+        data = klass.transform(data)
+    dropna = kwargs.get("dropna", False)
+    if dropna:
+        data = data[~np.isnan(data).any(axis=1)]
+    return data
 
 
 def polar_histogram(xdata, ydata, radial_bins="human", phi_bins=16, transformed=False, *args, **kwargs):
@@ -166,14 +231,13 @@ def polar_histogram(xdata, ydata, radial_bins="human", phi_bins=16, transformed=
     phi_range : Optional[tuple]
     range
     """
-    if not transformed:
-        rdata = np.hypot(ydata, xdata)
-        phidata = np.arctan2(ydata, xdata) % (2 * np.pi)
-    else:
-        rdata = xdata
-        phidata = ydata
-    data = np.concatenate([rdata[:, np.newaxis], phidata[:, np.newaxis]], axis=1)
-    dropna = kwargs.pop("dropna", False)
+    dropna = kwargs.pop("dropna", True)
+    data = np.concatenate([xdata[:, np.newaxis], ydata[:, np.newaxis]], axis=1)
+    print(data)
+    data = _prepare_data(data, transformed=transformed, klass=PolarHistogram, dropna=dropna)
+    print(data)
+    # print(data.shape)
+
     if isinstance(phi_bins, int):
         phi_range = (0, 2 * np.pi)
         if "phi_range" in "kwargs":
@@ -183,11 +247,7 @@ def polar_histogram(xdata, ydata, radial_bins="human", phi_bins=16, transformed=
         phi_range = list(phi_range) + [phi_bins + 1]
         phi_bins = np.linspace(*phi_range)
 
-    if dropna:
-        data = data[~np.isnan(data).any(axis=1)]
     bin_schemas = binnings.calculate_bins_nd(data, [radial_bins, phi_bins], *args, check_nan=not dropna, **kwargs)
-
-    # Prepare remaining data
     weights = kwargs.pop("weights", None)
     frequencies, errors2, missed = histogram_nd.calculate_frequencies(data, ndim=2,
                                                                   binnings=bin_schemas,
@@ -196,15 +256,10 @@ def polar_histogram(xdata, ydata, radial_bins="human", phi_bins=16, transformed=
 
 
 def spherical_histogram(data=None, radial_bins="human", theta_bins=16, phi_bins=16, transformed=False, *args, **kwargs):
-    if not transformed:
-        rdata, thetadata, phidata = SphericalHistogram.transform(data.T)
-    else:
-        rdata, thetadata, phidata = data.T
-    data = np.concatenate([rdata[:, np.newaxis], thetadata[:, np.newaxis], phidata[:, np.newaxis]], axis=1)
+    dropna = kwargs.pop("dropna", True)
+    data = _prepare_data(data, transformed=transformed, klass=SphericalHistogram, dropna=dropna)
 
-    dropna = kwargs.pop("dropna", False)
-    if dropna:
-        data = data[~np.isnan(data).any(axis=1)]
+    # TODO: Add arguments to construct bins
 
     bin_schemas = binnings.calculate_bins_nd(data, [radial_bins, theta_bins, phi_bins], *args,
                                              check_nan=not dropna, **kwargs)
--- tests/test_special.py
+++ tests/test_special.py
@@ -16,6 +16,42 @@ class TestPolar(object):
         assert h.bin_count == 8
         assert np.array_equal([[1, 0, 0, 0], [1, 1, 1, 0]], h.frequencies)
 
+    def test_transform(self):
+        t = special.PolarHistogram.transform([1, 0])
+        assert np.array_equal(t, [1, 0])
+
+        t = special.PolarHistogram.transform([0, 2])
+        assert np.allclose(t, [2, np.pi / 2])
+
+        data = np.asarray([[1, 0], [0, 2]])
+        t = special.PolarHistogram.transform(data)
+        assert np.allclose(t, [[1, 0], [2, np.pi / 2]])
+
+
+class TestSpherical(object):
+    def test_transform(self):
+        t = special.SphericalHistogram.transform([0, 0, 1])
+        assert np.array_equal(t,  [1, 0, 0])
+
+        t = special.SphericalHistogram.transform([2, 2, 0])
+        assert np.array_equal(t,  [np.sqrt(8), np.pi / 2, np.pi / 4])
+
+        data = np.asarray([[3, 0, 0], [0, 0, 0], [0, .5, -.5]])
+        expected = np.asarray([[3, np.pi / 2, 0], [0, 0, 0], [np.sqrt(.5), .75 * np.pi, np.pi / 2]])
+        assert np.allclose(expected, special.SphericalHistogram.transform(data))
+
+class TestCylindrical(object):
+    def test_transform(self):
+        t = special.CylindricalHistogram.transform([0, 0, 1])
+        assert np.array_equal(t,  [0, 0, 1])
+
+        t = special.CylindricalHistogram.transform([2, 2, 2])
+        assert np.array_equal(t,  [np.sqrt(8), np.pi / 4, 2])
+
+        data = np.asarray([[3, 0, 0], [0, 0, 0], [0, .5, -.5]])
+        expected = np.asarray([[3, 0, 0], [0, 0, 0], [.5, np.pi / 2, -.5]])
+        assert np.allclose(expected, special.CylindricalHistogram.transform(data))
+
 
     # def test_fill_n(self):
 
