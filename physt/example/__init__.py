9e6c1c9d33573d7c8688498789c962f57be69a32
Jan Pipek <jan.pipek@gmail.com>
2017-01-16 14:51:06 +0100

    Examples

--- physt/example/__init__.py
+++ physt/example/__init__.py
@@ -15,24 +15,34 @@ def normal_h2(size=10000):
     data2 = np.random.normal(0, 1, (size,))
     return h2(data1, data2)
 
-def iris_h1(x="sepal_length"):
-    """1D histogram of iris data."""
-    try:
-        import seaborn as sns
-    except ImportError:
-        raise RuntimeError("Cannot plot iris data, seaborn has to be present.")
-    iris = sns.load_dataset("iris")
-    return h1(iris[x], "human", 20, name="iris")
+ALL_EXAMPLES = [normal_h1, normal_h2]
+
+try:
+    import seaborn.apionly as sns
+
+    def iris_h1(x="sepal_length"):
+        """1D histogram of iris data."""
+        try:
+            import seaborn as sns
+        except ImportError:
+            raise RuntimeError("Cannot plot iris data, seaborn has to be present.")
+        iris = sns.load_dataset("iris")
+        return h1(iris[x], "human", 20, name="iris")
 
 
-def iris_h2(x="sepal_length", y="sepal_width"):
-    """1D histogram of iris data."""
-    try:
-        import seaborn as sns
-    except ImportError:
-        raise RuntimeError("Cannot plot iris data, seaborn has to be present.")
-    iris = sns.load_dataset("iris")
-    return h2(iris[x], iris[y], "human", 20, name="iris")
+    def iris_h2(x="sepal_length", y="sepal_width"):
+        """1D histogram of iris data."""
+        try:
+            import seaborn as sns
+        except ImportError:
+            raise RuntimeError("Cannot plot iris data, seaborn has to be present.")
+        iris = sns.load_dataset("iris")
+        return h2(iris[x], iris[y], "human", 20, name="iris")
+
+    ALL_EXAMPLES += [iris_h1, iris_h2]
+    
+except:
+    pass
 
 
-ALL_EXAMPLES = (normal_h1, normal_h2, iris_h1, iris_h2)
+
