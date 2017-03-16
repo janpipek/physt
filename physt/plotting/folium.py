c3e15bc8ff77b716a6bef3abe67724b815313532
Jan Pipek <jan.pipek@gmail.com>
2017-03-16 14:29:55 +0100

    Geospatial data example

--- physt/plotting/folium.py
+++ physt/plotting/folium.py
@@ -1,24 +1,35 @@
+"""
+Plotting inside maps with folium.
+
+Very experimental!
+"""
 from __future__ import absolute_import
 
 import folium
 
-types = ("map")
+types = ("geo_map")
 
 dims = {
-    "map" : [2],
+    "geo_map" : [2],
 }
 
-def bins_to_json(h2):
-    """
 
-    :type h2: physt.histogram_nd.Histogram2D
-    :param map:
-    :return:
+def _bins_to_json(h2):
+    """Create GeoJSON representation of histogram bins
+
+    Parameters
+    ----------
+    h2: physt.histogram_nd.Histogram2D
+        A histogram of coordinates (in degrees)
+
+    Returns
+    -------
+    geo_json : dict
     """
-    x0 = h2.get_bin_left_edges(0)
-    x1 = h2.get_bin_right_edges(0)
-    y0 = h2.get_bin_left_edges(1)
-    y1 = h2.get_bin_right_edges(1)
+    south = h2.get_bin_left_edges(0)
+    north = h2.get_bin_right_edges(0)
+    west = h2.get_bin_left_edges(1)
+    east = h2.get_bin_right_edges(1)
     return {
         "type":"FeatureCollection",
         "features": [
@@ -26,12 +37,13 @@ def bins_to_json(h2):
                 "type": "Feature",
                 "geometry": {
                     "type": "Polygon",
+                    # Note that folium and GeoJson have them swapped
                     "coordinates": [[
-                        [x0[i], y0[j]],
-                        [x1[i], y0[j]],
-                        [x1[i], y1[j]],
-                        [x0[i], y1[j]],
-                        [x0[i], y0[j]]]],
+                        [east[j], south[i]],
+                        [east[j], north[i]],
+                        [west[j], north[i]],
+                        [west[j], south[i]],
+                        [east[j], south[i]]]],
                 },
                 "properties" : {
                     "count": float(h2.frequencies[i, j])
@@ -43,27 +55,43 @@ def bins_to_json(h2):
     }
 
 
-def map(h2, map=None, tiles='stamentoner', cmap="wk", alpha=0.5, lw=1):
-    """
+def geo_map(h2, map=None, tiles='stamenterrain', cmap="wk", alpha=0.5, lw=1, fit_bounds=None, layer_name=None):
+    """Show rectangular grid over a map.
 
-    :type h2: physt.histogram_nd.Histogram2D
-    :param map:
-    :return:
+    Parameters
+    ----------
+    h2: physt.histogram_nd.Histogram2D
+        A histogram of coordinates (in degrees: latitude, longitude)
+    map : folium.folium.Map
+
+    Returns
+    -------
+    map : folium.folium.Map
     """
     if not map:
-        posx = h2.get_bin_centers(0).mean()
-        posy = h2.get_bin_centers(1).mean()
+        latitude = h2.get_bin_centers(0).mean()
+        longitude = h2.get_bin_centers(1).mean()
         zoom_start = 10
-        map = folium.Map(location=[posy, posx], tiles=tiles)
-    geo_json = bins_to_json(h2)
+        map = folium.Map(location=[latitude, longitude], tiles=tiles)
+        if fit_bounds == None:
+            fit_bounds = True
+
+    geo_json = _bins_to_json(h2)
+
+    if not layer_name:
+        layer_name = h2.name
 
     from branca.colormap import LinearColormap
 
     color_map = LinearColormap(cmap, vmin=h2.frequencies.min(), vmax=h2.frequencies.max())
 
-    #xx = h2.frequencies.max()
+    # legend = folium.Html("<div>Legend</div>")
+    # legend_div = folium.Div("20%", "20%", "75%", "5%")
+    #
+    # legend_div.add_to(map)
+    # legend_div.add_child(legend)
 
-    # folium.
+    #xx = h2.frequencies.max()
 
     def styling_function(bin):
         count = bin["properties"]["count"]
@@ -71,8 +99,13 @@ def map(h2, map=None, tiles='stamentoner', cmap="wk", alpha=0.5, lw=1):
             "fillColor": color_map(count),
             "color": "black",
             "fillOpacity": alpha if count > 0 else 0,
-            "strokeWidth": lw
+            "weight": lw,
+            # "strokeWidth": lw,
+            "opacity": alpha if count > 0 else 0,
         }# .update(styling)
 
-    folium.GeoJson(geo_json, style_function=styling_function).add_to(map)
+    layer = folium.GeoJson(geo_json, style_function=styling_function, name=layer_name)
+    layer.add_to(map)
+    if fit_bounds:
+        map.fit_bounds(layer.get_bounds())
     return map
