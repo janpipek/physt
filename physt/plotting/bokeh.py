43fedbaf5818362204345e669edc5f3a676e1496
Jan Pipek <jan.pipek@gmail.com>
2017-04-09 00:10:27 +0200

    bokeh map (basic)

--- physt/plotting/bokeh.py
+++ physt/plotting/bokeh.py
@@ -7,17 +7,18 @@ from __future__ import absolute_import
 from bokeh.plotting import figure
 from bokeh.charts import Scatter
 from bokeh.charts import show as bokeh_show
-from bokeh.models import HoverTool
+from bokeh.models import HoverTool, LinearColorMapper
 from bokeh.models.sources import ColumnDataSource
 import numpy as np
 
 from .common import get_data
 
-types = ("bar", "scatter")
+types = ("bar", "scatter", "map")
 
 dims = {
     "scatter" : [1],
     "bar" : [1],
+    "map" : [2]
 }
 
 
@@ -65,6 +66,31 @@ def bar(h1, show=True, **kwargs):
         line_alpha=kwargs.get("alpha", 1),
     )
 
+    if show:
+        bokeh_show(p)
+    return p
+    
+def map(h2, show=True, **kwargs):
+    density = kwargs.pop("density", False)
+    data = get_data(h2, density=density)
+    
+    source = ColumnDataSource({
+        "color": data.T.flatten()
+    })
+    
+    X, Y = np.meshgrid(h2.get_bin_left_edges(0), h2.get_bin_left_edges(1))
+    dX, dY = np.meshgrid(h2.get_bin_widths(0), h2.get_bin_widths(1))
+    #colors = ["#75968f", "#550b1d"]
+    from bokeh.palettes import Greys256
+    mapper = LinearColorMapper(palette=Greys256, low=data.max(), high=data.min())
+    
+    p = kwargs.get("figure", figure())
+    # TODO: Use ColumnDataSource fields
+    p.rect(x=Y.flatten(), y=X.flatten(), source=source,
+           width=dX.flatten(), height=dY.flatten(),
+           fill_color={"field" : "color", "transform" : mapper},
+           line_color="white")
+    
     if show:
         bokeh_show(p)
     return p
