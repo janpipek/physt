fbdc20caf4da22a218b1b416a9266a818623200b
Jan Pipek <jan.pipek@gmail.com>
2017-07-07 14:09:20 +0200

    Version 0.3.29

--- physt/__init__.py
+++ physt/__init__.py
@@ -12,7 +12,7 @@ See https://github.com/janpipek/physt
 from __future__ import absolute_import
 from . import binnings
 
-__version__ = str('0.3.28.1')
+__version__ = str('0.3.29')
 
 
 def histogram(data, bins=None, *args, **kwargs):
