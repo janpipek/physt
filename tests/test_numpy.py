d728a96fc5bb39f23c52dfbe59dc4fd922dab39a
Jan Pipek <jan.pipek@showmax.com>
2017-12-02 11:25:36 +0100

    Test: numpy with histogram2d

--- tests/test_numpy.py
+++ tests/test_numpy.py
@@ -8,6 +8,8 @@ from physt.compat.numpy import histogram as _histogram
 from physt.compat.numpy import histogram2d as _histogram2d
 from physt.compat.numpy import histogramdd as _histogram2d
 
+X = np.random.normal(0, 1, 100)
+Y = np.random.normal(0, 1, 100)
 
 class TestHistogramEqualness:
     def _test_with_args(self, array, *args, **kwargs):
@@ -19,35 +21,39 @@ class TestHistogramEqualness:
         assert np.array_equal(None, histogram.schema.mask)
 
     def test_no_args(self):
-        array = np.random.normal(0, 1, 100)
-        self._test_with_args(array)
+        self._test_with_args(X)
 
     def test_with_range(self):
         array = np.arange(0, 10, 100)
         self._test_with_args(array, range=(2, 3))
 
     def test_with_bin_number(self):
-        array = np.random.normal(0, 1, 100)
-        self._test_with_args(array, 47)
+        self._test_with_args(X, 47)
 
     def test_with_bin_strings(self):
-        array = np.random.normal(0, 0, 100)
         for algo in BIN_COUNT_ALGORITHMS:
-            self._test_with_args(array, algo)
+            self._test_with_args(X, algo)
 
     def test_with_fixed_bins(self):
-        array = np.random.normal(0, 1, 100)
         edges = [-0.1, 0, 0.2, 0.4, 0.7]
-        self._test_with_args(array, edges)
+        self._test_with_args(X, edges)
 
     def test_with_weights(self):
-        array = np.random.normal(0, 1, 100)
         weights = np.random.lognormal(0, 1, 100)
-        self._test_with_args(array, weights=weights)
+        self._test_with_args(X, weights=weights)
 
 
 class TestHistogram2dEqualness:
-    pass
+    def _test_with_args(self, x, y, *args, **kwargs):
+        values, edges = np.histogram2d(x, y, *args, **kwargs)
+        histogram = _histogram2d(x, y, *args, **kwargs)
+
+        assert np.array_equal(values, histogram.values)
+        assert np.array_equal(edges, histogram.schema.edges)
+        assert np.array_equal(None, histogram.schema.mask)
+
+    def test_no_args(self):
+        self._test_with_args(X, Y)
 
 
 class TestHistogram3dEqualness:
