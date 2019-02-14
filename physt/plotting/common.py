cb6e501c41a2a41fb74714b8f06c4b40b7c0332f
Jan Pipek <jan.pipek@showmax.com>
2019-02-14 10:51:22 +0100

    TimeTickHandler - WIP

--- physt/plotting/common.py
+++ physt/plotting/common.py
@@ -1,7 +1,14 @@
 """
-Functions that are shared by several (all) plotting backends.
+Funct;ions that are shared by several (all) plotting backends.
 
 """
+import re
+from typing import Tuple, List, Union
+from datetime import timedelta, time
+
+import numpy as np
+
+from physt.histogram1d import Histogram1D
 
 
 def get_data(histogram, density=False, cumulative=False, flatten=False):
@@ -101,3 +108,97 @@ def pop_kwargs_with_prefix(prefix, kwargs):
     """
     keys = [key for key in kwargs if key.startswith(prefix)]
     return {key[len(prefix):]: kwargs.pop(key) for key in keys}
+
+
+TickCollection = Tuple[List[float], List[str]]
+
+
+class TimeTickHandler:
+    """
+
+    Note: This class is very experimental and subject to change or disappear.
+    """
+
+    def __init__(self, level=None, format=None):
+        self.level = self.parse_level(level) if level else None
+        self.format = format
+
+    LEVELS = {
+        "sec": 1,
+        "min": 60,
+        "hour": 3600,
+    }
+
+    LevelType = Tuple[str, int]
+
+    @classmethod
+    def parse_level(cls, value: Union[LevelType, float, str, timedelta]) -> LevelType:
+        if isinstance(value, tuple):
+            if len(value) != 2:
+                raise ValueError("Invalid level: {0}".format(value))
+            if value[0] not in cls.LEVELS:
+                raise ValueError("Invalid level: {0}".format(value))
+            if not isinstance(value[1], (float, int)):
+                raise ValueError("Invalid level: {0}".format(vaiue))
+            return value
+        elif isinstance(value, (float, int)):
+            ...
+        elif isinstance(value, timedelta):
+            ...
+        elif isinstance(value, str):
+            matchers = (
+                ("^([0-9]+)?h(our(s)?)?$", lambda m : ("hour", int(m[1] or 1))),
+                ("^([0-9]+)?m(in(s)?)?$", lambda m : ("min", int(m[1] or 1))),
+                ("^([0-9]+)?(\.[0-9]+)?s(ec(s)?)?$", lambda m : ("sec", float(m[1] or 1) + float("0." + (m[2] or "0")))),
+            )
+            for matcher in matchers:
+                match = re.match(matcher[0], value)
+                if match:
+                    return matcher[1](match)
+            raise ValueError("Cannot parse level: {0}".format(value))
+        else:
+            raise ValueError("Invalid level: {0}".format(vaiue))
+
+    @classmethod
+    def deduce_level(cls, h1: Histogram1D) -> str:
+        return ("min", 1)
+        # TODO: really?
+
+    def get_time_ticks(self, h1: Histogram1D, level, min_: float, max_: float) -> List[float]:
+        width = level[1] * self.LEVELS[level[0]]
+        min_factor = int(min_ // width)
+        if min_ % width != 0:
+            min_factor += 1
+        max_factor = int(max_ // width)
+        return list(np.arange(min_factor, max_factor + 1) * width)
+        
+    @classmethod
+    def split_hms(cls, value) -> Tuple[int, int, float]:
+        ...
+    
+    def format_time_ticks(self, ticks: List[float]) -> List[str]:
+        ...
+        # return [str(tick) for tick in ticks]
+        
+        # deltas = [self.split_hms(tick) for tick in ticks]
+        # if self.format:
+        #     format = self.format
+        # else:
+        #     include_micros = any(delta.microseconds for delta in deltas)
+        #     include_secs = any(delta.seconds % 60 for delta in deltas) or include_micros
+        #     include_hours = any(delta.total_seconds() >= 3600 for delta in deltas)
+        #     include_minutes = include_hours or any(delta.total_seconds() >= 60 for delta in deltas)
+        #     # format = "%H:%M:%S"
+        #     format = ""
+        #     format += "%H:" if include_hours else ""
+        #     format += "%M" if include_minutes else ""
+        #     format += ":%S" if include_secs else ""
+        #     format += ".%f" if include_micros else ""
+        #     format += "}"
+        # return [format.format(time(delta)) for delta in deltas]
+
+    def __call__(self, h1: Histogram1D, min_: float, max_: float) -> TickCollection:
+        level = self.level or cls.deduce_level(h1)
+        ticks = self.get_time_ticks(h1, level, min_, max_)
+        tick_labels = self.format_time_ticks(ticks)
+        return ticks, tick_labels 
