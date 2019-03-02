51c360d30251a1ab57b79717bb8161b7b07c6c72
Jan Pipek <jan.pipek@showmax.com>
2019-03-02 17:47:38 +0100

    A bit more advanced plotly with collections

--- physt/plotting/plotly.py
+++ physt/plotting/plotly.py
@@ -8,14 +8,31 @@ Currently, it uses matplotlib translation for 1D histograms:
 TODO: More elaborate output planned
 """
 from functools import wraps
+from typing import Any, Optional, Union
 
+import plotly.offline as pyo
+import plotly.plotly as pyp
+import plotly.graph_objs as go
+
+from physt.histogram1d import Histogram1D
 from . import matplotlib as mpl_backend
 
 
-def _wrap_matplotlib_f(f):
-    import plotly.offline as pyo
-    import plotly.plotly as pyp
+def _wrap_standard_f(f):
+    @wraps(f)
+    def new_f(*args, raw=False, offline=True, write_to=None, **kwargs):
+        py = pyo if offline else pyp
+        object = f(*args, **kwargs)
+        if raw:
+            return object
+        if write_to:
+            return py.plot(object, filename=write_to)
+        else:
+            return py.iplot(object)
 
+    return new_f
+
+def _wrap_matplotlib_f(f):
     @wraps(f)
     def new_f(*args, offline=True, write_to=None, **kwargs):
         py = pyo if offline else pyp
@@ -29,11 +46,102 @@ def _wrap_matplotlib_f(f):
     return new_f
 
 
+def wrap(*, mpl_function: Optional[Any] = None):
+    def decorate(function):
+        normal_variant = _wrap_standard_f(function)
+        if mpl_function:
+            mpl_variant = _wrap_matplotlib_f(mpl_function)
+
+            @wraps(function)
+            def new_f(*args, mpl: bool = False, **kwargs):
+                if mpl:
+                    return mpl_variant(*args, **kwargs)
+                else:
+                    return normal_variant(*args, **kwargs)
+            return new_f
+        else:
+            return normal_variant
+
+    return decorate
+
+
+def enable_collection(f):
+    @wraps(f)
+    def new_f(h: Union[Histogram1D, "HistogramCollection"], **kwargs):
+        from physt.histogram_collection import HistogramCollection
+        if isinstance(h, HistogramCollection):
+            return f(h, **kwargs)
+        else:
+            return f(HistogramCollection(h), **kwargs)
+    return new_f
+
+
+def _add_ticks(xaxis: go.layout.XAxis, histogram: Histogram1D, kwargs: dict):
+    """Customize ticks for an axis (1D histogram)."""
+    ticks = kwargs.pop("ticks", None)
+    tick_handler = kwargs.pop("tick_handler", None)
+
+    if tick_handler:
+        if ticks:
+            raise ValueError("Cannot specify both tick and tick_handler")
+        ticks, labels = tick_handler(histogram, histogram.min_edge, histogram.max_edge)
+
+        xaxis.tickvals = ticks
+        xaxis.ticktext = labels
+
+    elif ticks == "center":
+        xaxis.tickvals = histogram.bin_centers
+    elif ticks == "edge":
+        xaxis.tickvals = histogram.bin_left_edges
+    else:
+        xaxis.tickvals = ticks
+
+
+@enable_collection
+def _line_or_scatter(h: "HistogramCollection", *, mode: str, **kwargs):
+    data = [go.Scatter(
+        x = histogram.bin_centers,
+        y = histogram.frequencies,
+        mode = mode,
+        name = histogram.name
+    ) for histogram in h]
+
+    layout = go.Layout()
+
+    _add_ticks(layout.xaxis, h[0], kwargs)
+
+    figure = go.Figure(data=data, layout=layout)
+    return figure
+
+
+@wrap(mpl_function=mpl_backend.scatter)
+def scatter(h, **kwargs):
+    return _line_or_scatter(h, mode="markers", **kwargs)
+
+
+@wrap(mpl_function=mpl_backend.line)
+@enable_collection
+def line(h, **kwargs):
+    return _line_or_scatter(h, mode="lines", **kwargs)
+
+
+# @_wrap_f
+# def line(h: Histogram1D):
+#     trace = go.Scatter(
+#         x = h.bin_centers,
+#         y = h.frequencies
+#     )
+#     return trace
+
+
+
+
+
 types = ["bar", "scatter", "line"]
 dims = {x : [1]  for x in types}
 
 for plot_type in types:
-    globals()[plot_type] = _wrap_matplotlib_f(mpl_backend.__dict__[plot_type])
+    if not plot_type in globals():
+        globals()[plot_type] = _wrap_matplotlib_f(mpl_backend.__dict__[plot_type])
     
-    
-del wraps, _wrap_matplotlib_f, mpl_backend, plot_type
+
