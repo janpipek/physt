ad79142f00780ee4bea5b1e615ed7e1c7d68c38d
Jan Pipek <jan.pipek@showmax.com>
2019-03-04 11:07:21 +0100

    Plotly: Basic "native" bar plot

--- physt/plotting/plotly.py
+++ physt/plotting/plotly.py
@@ -14,8 +14,18 @@ import plotly.offline as pyo
 import plotly.plotly as pyp
 import plotly.graph_objs as go
 
-from physt.histogram1d import Histogram1D
+from physt.histogram1d import Histogram1D, HistogramBase
+from physt.histogram_collection import HistogramCollection
+from physt.util import pop_many
 from . import matplotlib as mpl_backend
+from .common import get_data
+
+
+AbstractHistogram1D = Union[HistogramCollection, Histogram1D]
+
+
+DEFAULT_BARMODE = "overlay"
+DEFAULT_ALPHA = 1.0
 
 
 def _wrap_standard_f(f):
@@ -31,6 +41,7 @@ def _wrap_standard_f(f):
             return py.iplot(object)
 
     return new_f
+
 
 def _wrap_matplotlib_f(f):
     @wraps(f)
@@ -76,7 +87,7 @@ def enable_collection(f):
     return new_f
 
 
-def _add_ticks(xaxis: go.layout.XAxis, histogram: Histogram1D, kwargs: dict):
+def _add_ticks(xaxis: go.layout.XAxis, histogram: HistogramBase, kwargs: dict):
     """Customize ticks for an axis (1D histogram)."""
     ticks = kwargs.pop("ticks", None)
     tick_handler = kwargs.pop("tick_handler", None)
@@ -98,12 +109,13 @@ def _add_ticks(xaxis: go.layout.XAxis, histogram: Histogram1D, kwargs: dict):
 
 
 @enable_collection
-def _line_or_scatter(h: "HistogramCollection", *, mode: str, **kwargs):
+def _line_or_scatter(h: AbstractHistogram1D, *, mode: str, **kwargs):
+    get_data_kwargs = pop_many(kwargs, "density", "cumulative", "flatten")
     data = [go.Scatter(
-        x = histogram.bin_centers,
-        y = histogram.frequencies,
-        mode = mode,
-        name = histogram.name
+        x=histogram.bin_centers,
+        y=get_data(histogram, **get_data_kwargs),
+        mode=mode,
+        name=histogram.name
     ) for histogram in h]
 
     layout = go.Layout()
@@ -120,28 +132,46 @@ def scatter(h, **kwargs):
 
 
 @wrap(mpl_function=mpl_backend.line)
-@enable_collection
 def line(h, **kwargs):
     return _line_or_scatter(h, mode="lines", **kwargs)
 
 
-# @_wrap_f
-# def line(h: Histogram1D):
-#     trace = go.Scatter(
-#         x = h.bin_centers,
-#         y = h.frequencies
-#     )
-#     return trace
+@wrap(mpl_function=mpl_backend.bar)
+@enable_collection
+def bar(h, *,
+        barmode: str = DEFAULT_BARMODE,
+        alpha: float = DEFAULT_ALPHA,
+        **kwargs):
+    """
 
+    Parameters
+    ----------
+    alpha: Opacity (0.0 - 1.0)
+    barmode : "overlay" | "group" | "stack"
+    """
+    get_data_kwargs = pop_many(kwargs, "density", "cumulative", "flatten")
+    data = [go.Bar(
+        x=histogram.bin_centers,
+        y=get_data(histogram, **get_data_kwargs),
+        width=histogram.bin_widths,
+        name=histogram.name,
+        opacity=alpha,
+        **kwargs
+    ) for histogram in h]
 
+    layout = go.Layout(barmode=barmode)
 
+    _add_ticks(layout.xaxis, h[0], kwargs)
+
+    figure = go.Figure(data=data, layout=layout)
+    return figure
 
 
 types = ["bar", "scatter", "line"]
-dims = {x : [1]  for x in types}
+dims = {x:[1] for x in types}
 
-for plot_type in types:
-    if not plot_type in globals():
-        globals()[plot_type] = _wrap_matplotlib_f(mpl_backend.__dict__[plot_type])
+# for plot_type in types:
+#     if plot_type not in globals():
+#         globals()[plot_type] = _wrap_matplotlib_f(mpl_backend.__dict__[plot_type])
     
 
