be08950a294f34478f44e224eb6dd04ebf37cfd7
Jan Pipek <jan.pipek@gmail.com>
2020-02-17 21:32:38 +0100

    Bugfix version

--- physt/version.py
+++ physt/version.py
@@ -1,5 +1,5 @@
 """Package information."""
-__version__ = "0.4.10"
+__version__ = "0.4.10.1"
 __author__ = "Jan Pipek"
 __author_email__ = "jan.pipek@gmail.com"
 __url__ = "https://github.com/janpipek/physt"
