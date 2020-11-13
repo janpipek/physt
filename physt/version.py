c7f71b5dcebc5536f45ca8e26e7f497259076566
Jan Pipek <jan.pipek@gmail.com>
2020-11-13 11:57:43 +0100

    new version

--- physt/version.py
+++ physt/version.py
@@ -1,5 +1,5 @@
 """Package information."""
-__version__ = "0.4.12.1"
+__version__ = "0.4.12.2"
 __author__ = "Jan Pipek"
 __author_email__ = "jan.pipek@gmail.com"
 __url__ = "https://github.com/janpipek/physt"
