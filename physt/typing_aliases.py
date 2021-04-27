a0863bdc217007871026656130a695110c3c88dd
Jan Pipek <jan.pipek@gmail.com>
2021-04-27 09:25:29 +0200

    Annotate a bit

--- physt/typing_aliases.py
+++ physt/typing_aliases.py
@@ -1,11 +1,22 @@
 """Definitions for type hints."""
-from typing import Tuple, Union, Iterable
+from typing import Iterable, Tuple, Type, Union
 
 from numpy import ndarray, dtype
 
 RangeTuple = Tuple[float, float]
 Axis = Union[int, str]
 
-# TODO: Update with numpy 1.20
 ArrayLike = Union[ndarray, Iterable, int, float]
 DtypeLike = Union[type, dtype, str]
+
+# ArrayLike: Type
+# DtypeLike: Type
+
+# if np.__version__ >= "1.20":
+#     ArrayLike = typing.ArrayLike
+#     DTypeLike = typing.DTypeLike
+# else:
+#     ArrayLike = Union[ndarray, Iterable, int, float]
+#     DTypeLike = Union[type, dtype, str]
+
+__all__ = ["RangeTuple", "Axis", "ArrayLike", "DtypeLike"]
