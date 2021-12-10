ce2dabb82cb26ca69e4e45a7cc81578f8583067e
Jan Pipek <jan.pipek@gmail.com>
2021-12-10 15:51:31 +0100

    require typing_extensions

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ options = dict(
     author_email=INFO["__author_email__"],
     url=INFO["__url__"],
     package_data={"physt": ["examples/*.csv"]},
-    install_requires=["numpy>=1.17", "packaging"],
+    install_requires=["numpy>=1.17", "packaging", "typing_extensions"],
     python_requires="~=3.6",
     extras_require={
         "all": [
