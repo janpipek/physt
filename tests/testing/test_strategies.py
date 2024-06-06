d25bee82c339f19bf8b474e55cb3c46de4618300
Jan Pipek <jan.pipek@gmail.com>
2024-06-06 14:06:38 +0200

    Remove warning

--- tests/testing/test_strategies.py
+++ tests/testing/test_strategies.py
@@ -1,12 +1,16 @@
+from hypothesis import given
+
 from physt.testing.strategies import histograms_1d, histograms_nd
 from physt.types import Histogram1D, HistogramND
 
 
 class TestHistograms1D:
-    def test_it_works(self):
-        assert isinstance(histograms_1d().example(), Histogram1D)
+    @given(example=histograms_1d())
+    def test_it_works(self, example):
+        assert isinstance(example, Histogram1D)
 
 
 class TestHistogramsND:
-    def test_it_works(self):
-        assert isinstance(histograms_nd().example(), HistogramND)
+    @given(example=histograms_nd())
+    def test_it_works(self, example):
+        assert isinstance(example, HistogramND)
