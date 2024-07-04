1ffec8ea5395b524c861feba6509356779bda4f0
Jan Pipek <jan.pipek@gmail.com>
2024-07-04 10:03:49 +0200

    A bit more of the examples

--- src/physt/examples/__main__.py
+++ src/physt/examples/__main__.py
@@ -4,24 +4,39 @@ You can run this like:
 
     python -m physt.examples
 """
-from rich.pretty import pprint
+import importlib
+import sys
+
+if not importlib.util.find_spec("rich"):
+    print("Please, install rich to view nice terminal output.")
+    sys.exit(-1)
+
+from rich.console import Console
 
 from physt.examples import normal_h1, normal_h2
+
+console = Console()
+
 
 h1 = normal_h1()
 h2 = normal_h2()
 
-print("A 1D histogram: ")
-pprint(h1)
+console.print("[bold]A 1D histogram:[/bold]")
+console.print(h1)
 
-print()
-print("Its stats:")
-pprint(h1.statistics)
+console.print()
+console.print("[bold]The associated stats:[/bold]")
+console.print(h1.statistics)
 
-print()
+console.print()
+console.print("[bold]And the plot:[/bold]")
+
 h1.plot(backend="ascii", show_values=True)
+console.print()
 
-print("A 2D histogram: ")
-pprint(h2)
+console.print("[bold]A 2D histogram:[/bold]")
+console.print(h2)
 
+console.print()
+console.print("[bold]And the plot:[/bold]")
 h2.plot(backend="ascii")
