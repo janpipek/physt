f3f2dcfc5ecade78461a7998f27ecf77c5e3f493
Jan Pipek <jan.pipek@gmail.com>
2024-10-11 14:55:14 +0200

    Bump version: v0.8.3 → v0.8.4

--- src/physt/version.py
+++ src/physt/version.py
@@ -1,5 +1,5 @@
 """Package information."""
-__version__ = "0.8.2"
+__version__ = "0.8.4"
 __author__ = "Jan Pipek"
 __author_email__ = "jan.pipek@gmail.com"
 __url__ = "https://github.com/janpipek/physt"
