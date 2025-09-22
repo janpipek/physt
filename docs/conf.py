5372e0c2781d9fe27a46dd3a6d3c30885608c804
Jan Pipek <jan.pipek@gmail.com>
2025-09-22 09:25:42 +0200

    Fix ipython highlighting

--- docs/conf.py
+++ docs/conf.py
@@ -35,6 +35,7 @@ extensions = [
     "sphinx.ext.mathjax",
     "sphinx.ext.napoleon",
     "nbsphinx",
+    "IPython.sphinxext.ipython_console_highlighting"
 ]
 
 # Add any paths that contain templates here, relative to this directory.
